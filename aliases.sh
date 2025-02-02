c4c76e8a8cf98e7f3599f49a439a38e46ae8edc7
rsupatrio <rsupatrio@gmail.com>
2025-02-02 21:55:22 +0800

    Add aliases

---
+++ aliases.sh
@@ -0,0 +1,13 @@
+# Loop through all files in the ~/.config/fabric/patterns directory
+for pattern_file in $HOME/.config/fabric/patterns/*; do
+    # Get the base name of the file (i.e., remove the directory path)
+    pattern_name=$(basename "$pattern_file")
+
+    # Create an alias in the form: alias pattern_name="fabric --pattern pattern_name"
+    alias_command="alias $pattern_name='fabric --pattern $pattern_name'"
+
+    # Evaluate the alias command to add it to the current shell
+    eval "$alias_command"
+done
+
+alias cbpaste="echo $(powershell.exe Get-Clipboard)"
